9b6021b2732f2fec328ff99252c34aee091c8698
Muhammad Ali <sayedalibukhari68@gmail.com>
2026-08-24 17:59:13 +0500

    Add authentication API setup

---
+++ src/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const connectDB = require("./config/database");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "StockMates API is running",
+  });
+});
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`StockMates server running on http://localhost:${PORT}`);
+});
